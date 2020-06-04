892d707e24de1a6960620da1af53fd62391d14e7
loickouao <kouao.emissan@etu.unige.ch>
2020-06-04 14:22:03 +0200

    Discover FactoryBoy

--- djangoapp/tests.py
+++ djangoapp/tests.py
@@ -15,15 +15,16 @@ def test_stock_model():
     
     assert stock.symbol == "Test Stock Symbol"
 
+
 @pytest.mark.django_db
-def test_quote_creation():
+def test_price_model():
     # create stock and price model instances
     stock = StockFactory(symbol="Test Stock Symbol")
-    price = PriceFactory(open_price=90.00, high_price=190.00, low_price=20.20, price=100.00, volume=40859, date="2020-06-03")
+    price = PriceFactory(open_price=90.00, high_price=190.00, low_price=20.20, price=100.00, volume=40859, date="2020-06-03", stock = stock)
 
     assert price.stock == stock
     assert price.stock.symbol == "Test Stock Symbol"
-    assert price.open_price == 100.00
+    assert price.open_price == 90.00
     assert price.high_price == 190.00
     assert price.low_price == 20.20
     assert price.price == 100.00
