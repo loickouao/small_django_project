a2e18f0e62ebcaa5bfdfb0c051887da55f40899b
loickouao <kouao.emissan@etu.unige.ch>
2020-06-04 05:21:06 +0200

    Discover FactoryBoy

---
+++ djangoapp/factories.py
@@ -0,0 +1,13 @@
+import factory
+from .models import Stock, Price
+
+class StockFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = Stock
+
+
+class PriceFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = Price
+        
+    stock = factory.SubFactory(StockFactory)
